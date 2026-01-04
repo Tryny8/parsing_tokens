ea8dc6f33753241a84d8c7b3aad66f9e511c0560
Antony Vitry <antony.vitry@fms-ea.com>
2026-01-04 19:39:55 +0100

    [feat(registry)] add extensible key_pattern registry

--- parsing_tokens/build_parser/register_parser.py
+++ parsing_tokens/build_parser/register_parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+KEYS_PATTERN = ["type_name", "balise_name", "separateur_name", "type_display"]
 TOKEN_TYPES = {"token", "ast", "interpreter"}
 BALISE_TYPES = {"parenthese", "bracket", "crochet"}
 SEPARATORS = {"deux_points", "egale"}
