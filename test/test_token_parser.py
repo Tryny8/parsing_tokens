fa5332fd34605fc862c924ee654178983131d097
Antony Vitry <antony.vitry@fms-ea.com>
2026-01-04 16:43:29 +0100

    [test(Token_parser)] add custom function for set and get token / balise / separator

--- test/test_token_parser.py
+++ test/test_token_parser.py
@@ -5,11 +5,24 @@
 """
 Classe TokenParser
 """
-import sys
-print(sys.path)
+
+
+import re
 
 from parsing_tokens.parser import TokenParser
 from parsing_tokens.build_io.api_io import open_file
+from parsing_tokens.build_parser.api_parser import set_register_token_type, create_token_pattern
+
+
+def test_custom_token_type():
+    set_register_token_type("custom")
+    pattern = create_token_pattern("custom", "bracket", "egale")
+    assert re.findall(pattern, "[custom=hello]")
+
+
+def test_default_token_still_works():
+    pattern = create_token_pattern("token", "bracket", "egale")
+    assert re.findall(pattern, "[token=test]")
 
 
 def main(verbose=False):
@@ -31,6 +44,18 @@ def main(verbose=False):
         print(">>", line, value, value[0].strip("()").split(":"))
 
     print("All Tokens parenthese:", tokens)
+
+    parser.add_token_type("task")
+    parser.add_balise("chevron", open_char="<", close_char=">")
+    parser.add_separator("tiret", "-")
+
+    print(parser.get_all_token_type())
+    print(parser.get_all_balise())
+    print(parser.get_all_separator())
+
+
+    test_custom_token_type()
+    test_default_token_still_works()
 
 
 if __name__ == "__main__":
